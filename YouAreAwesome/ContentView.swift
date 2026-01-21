376360b5a84bb24da6fbd50f638f3f5b150d2741
Michiel <Michielnooij@icloud.com>
2026-01-21 20:06:56 +0100

    Changed to conditionals

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,27 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var message = "You are Awesome !"
+    @State private var exampleText = ""
+    @State private var exampleImage = ""
     
     var body: some View {
         
         VStack{
-            Image(systemName: "swift")
+            Spacer()
+            Image(systemName: exampleImage)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text(message)
+            Text(exampleText)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            
-            Button{
-                message = "I am a programmer"
+                .fontWeight(.ultraLight)
+            Spacer()
+            Button("Push Me"){
+                
+                let message1 = "You are Awesome !"
+                let message2 = "You are Great !"
+                let image1 = "hand.thumbsup"
+                let image2 = "sun.max.fill"
+                
+                if exampleText == "You are Awesome !" {
+                    exampleText = message2
+                    exampleImage = image2
+                } else {
+                    exampleText = message1
+                    exampleImage = image1
+                }
+                
+                
             }
-        label: {
-                Text("Click Me !")
-            }
+            .buttonStyle(.glassProminent)
+            .font(.title2)
+            .tint(.orange)
         }
+        .padding()
         
         
     }
